fe22568a42850b394cfa7eaa146202d955d07f31
Farai Major <faraimutukumira@gmail.com>
2022-01-01 13:38:40 -0500

    modified problem 11

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,16 +1,7 @@
 function sumOfMinimumAndMaximum(nums) {
   // Insert code here;
-  let min = nums[0];
-  let max = nums[0];
-  for(let i = 0; i < nums.length; i++){
-    if(min > nums[i]){
-      min = nums[i];
-    }
-    if(max < nums[i]){
-      max = nums[i]
-    }
-  }
-  return max + min;
+
+  return Math.max(...nums) + Math.min(...nums);
 }
 
 // Do not edit this line;
